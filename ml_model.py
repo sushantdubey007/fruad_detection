f3a8629eece54ef8eb1914ca4f2c91c58aa2c42d
sushantdubey <dubeysushant007@gmail.com>
2024-10-22 23:26:14 +0530

    added 3 new ml model code

--- ml_model.py
+++ ml_model.py
@@ -1,1 +1,10 @@
 print("adding all ML related code")
+
+def model1():
+    print("adding code for ML model 1")
+
+def model2():
+    print("adding code for ML model 2")
+
+def model3():
+    print("adding code for ML model 3") 
