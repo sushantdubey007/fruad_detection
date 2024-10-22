8a5e0c498d57c205e5a24dbda20f94fd27ba6a78
sushantdubey <dubeysushant007@gmail.com>
2024-10-22 23:27:29 +0530

    model 2 is the final model

--- ml_model.py
+++ ml_model.py
@@ -7,4 +7,6 @@ def model2():
     print("adding code for ML model 2")
 
 def model3():
-    print("adding code for ML model 3") 
+    print("adding code for ML model 3")
+
+print("model 2 is final") 
